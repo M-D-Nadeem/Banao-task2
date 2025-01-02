ca64456dad7270495553be7a63cf0b5fa9986c01
M-D-Nadeem <149703432+M-D-Nadeem@users.noreply.github.com>
2025-01-03 05:22:50 +0530

    add

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,32 +1,22 @@
 import multer from "multer";
 import path from "path"
-const upload = multer(
-    { dest: 'uploads',
-    limits:{fieldSize:759476803 *1000000}, 
-    storage : multer.diskStorage({
-        destination: function (req, file, cb) {
-          cb(null, 'uploads/')
-        },
-        filename: function (req, file, cb) {
-          cb(null, file.originalname)
-        }
-      }),   
-
-fileFilter:(req,file,cb)=>{
-    let ext=path.extname(file.originalname)
-
-    if(
-        ext!==".jpg"&&
-        ext!==".jpeg"&&
-        ext!==".webp"&&
-        ext!==".png"&&
-        ext!==".jfif"
-        
-    ){
-      return cb(new Error(`Unsupported file type ${ext}`),false)
-        
+const uploadDir = path.resolve("uploads"); // Absolute path
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, uploadDir); // Use absolute path
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+  limits: { fieldSize: 759476803 * 1000000 },
+  fileFilter: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    if (![".jpg", ".jpeg", ".webp", ".png", ".jfif"].includes(ext)) {
+      return cb(new Error(`Unsupported file type ${ext}`), false);
     }
-    cb(null, true)
-}
-})
+    cb(null, true);
+  },
+});
 export default upload
